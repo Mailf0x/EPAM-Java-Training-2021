e66de183e6bbfc3d803c2a2bb61f44f1b1579bd5
Vital Nazarov <75082699+Mailf0x@users.noreply.github.com>
2021-06-09 14:17:46 +0300

    Create Product.java

---
+++ Task_5/src/domain/Product.java
@@ -0,0 +1,22 @@
+package domain;
+
+public class Product extends Entity {
+	private Double cost;
+	private String name;
+
+	public Double getCost() {
+		return cost;
+	}
+
+	public void setCost(Double cost) {
+		this.cost = cost;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
